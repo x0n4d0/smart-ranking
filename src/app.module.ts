d101fda10ec51b5d3e130a468e48625ab2fe7277
x0n4d0 <romagnolimauricio@gmail.com>
2022-04-10 21:57:11 -0300

    feat: start players module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [],
+  imports: [PlayersModule],
 })
 export class AppModule {}
